779ff0b0129329553a89c14bdf769f4b9792cf04
larod <LAROD@LAROD-MacbookPro.local>
2014-06-30 15:22:52 -0400

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/Grid.m
+++ GameOfLife.spritebuilder/Source/Grid.m
@@ -49,6 +49,7 @@ static const int GRID_COLUMNS = 10;
             _gridArray[i][j] = creature;
             
             // make creatures visible to test this method, remove this once we know we have filled the grid properly
+            x+=_cellWidth;
         }
         
         y += _cellHeight;
